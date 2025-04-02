d61859698fc5caaec11749d31658dc5bfa8fcabd
davidecondoluci <151521993+davidecondoluci@users.noreply.github.com>
2025-04-02 19:46:51 +0200

    new logo

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,10 +1,14 @@
-import styles from "./Header.module.css"
+import styles from "./Header.module.css";
 
 export const Header = () => {
   return (
     <div className={styles.wrapper}>
       <header className={styles.header}>
-        <h1 className={styles.title}>Rick & Morty</h1>
+        <a href="/">
+          <h1>
+            <img src="/logo.svg" />
+          </h1>
+        </a>
         <small>Catalogo personaggi</small>
       </header>
     </div>
